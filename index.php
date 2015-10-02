9ddbb45186be094a0d9842fb6b02a3ffbba98d0e
MarrySunshine <wei1996zero@hotmail.com>
2015-10-02 19:16:32 +0800

    管理页

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>鲁厶维的Modern Web Programming作业</title>
+    <link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+    <img id="background" src="./background.jpg" />
+    <h1 id="head">作业</h1>
+    <?php
+        $filename = scandir("./");
+        for ($i = 0; $i < sizeof($filename); ++$i) {
+            if (is_dir($filename[$i]) && $filename[$i][0] != '.') {
+                echo '<a href="' . $filename[$i] . '"><p>' . $filename[$i] . "</p></a><br/>";
+            }
+        }
+    ?>
+</body>
+</html>
