860941936cc52b4f7aa9152029d5bf8b626132e0
MarrySunshine <wei1996zero@hotmail.com>
2015-10-10 22:32:56 +0800

    apple

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <title>鲁厶维的Modern Web Programming作业</title>
     <link rel="stylesheet" href="./index.css"/>
 </head>
